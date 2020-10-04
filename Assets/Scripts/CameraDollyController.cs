9ee3761745ea03558383f0bd65a771854b5080a5
GameDevChef <elevatorcoding@gmail.com>
2020-10-04 18:13:52 +0200

    Removed DOF controll

--- Assets/Scripts/CameraDollyController.cs
+++ Assets/Scripts/CameraDollyController.cs
@@ -9,16 +9,13 @@ using UnityEngine.Rendering.PostProcessing;
 public class CameraDollyController : MonoBehaviour
 {
     [SerializeField] private bool followTarget;
-    [SerializeField] private bool followTargetWithDOF;
     private CinemachineDollyCart cart;
     private CinemachineVirtualCamera virtualCamera;
-    private CinemachinePostProcessing cinemachinePostProcessing;
 
     private void Awake()
     {
         cart = GetComponent<CinemachineDollyCart>();
         virtualCamera = GetComponentInChildren<CinemachineVirtualCamera>();
-        cinemachinePostProcessing = GetComponentInChildren<CinemachinePostProcessing>();
     }
 
     public void InitDolly(CinemachineSmoothPath dollyTrack, Transform target, float speed = 0f)
@@ -28,26 +25,6 @@ public class CameraDollyController : MonoBehaviour
         cart.m_Path = dollyTrack;
         if(followTarget)
             virtualCamera.m_LookAt = target;
-        if(cinemachinePostProcessing != null)
-        {
-            DepthOfField depth;
-            cinemachinePostProcessing.m_Profile.TryGetSettings<DepthOfField>(out depth);
-            if(depth != null)
-            {
-                if (followTargetWithDOF)
-                {
-                    depth.active = true;
-                    cinemachinePostProcessing.m_FocusTarget = target;
-                }
-                else
-                {
-                    depth.active = false;
-                }
-            }
-            else
-            {
-                depth.active = false;
-            }
-        }           
+        
     }
 }
